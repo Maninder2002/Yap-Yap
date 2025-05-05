9ec40242f70e9181cbeb6c0c9e4cfe346bf8efa0
Maninder2002 <manindersahdev0020@gmail.com>
2025-05-05 23:39:44 +0530

    Initial commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,8 +9,8 @@ export const generateToken = (userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000,
         httpOnly:true,
-        sameSite:true,
-        secure:process.env.Node_ENV!== "development"
+        sameSite:'lax',
+        secure:process.env.NODE_ENV!== "development"
     });
 
     return token

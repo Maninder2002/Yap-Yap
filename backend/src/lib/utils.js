b671bbae6faff7d2fda03f1f4120c7146b6d4b07
Maninder2002 <manindersahdev0020@gmail.com>
2025-05-06 00:23:42 +0530

    Initial commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,7 +10,7 @@ export const generateToken = (userId,res)=>{
         maxAge:7*24*60*60*1000,
         httpOnly:true,
         sameSite:true,
-        secure:process.env.NODE_ENV!== "development"
+        secure:process.env.NODE_ENV!== "production"
     });
 
     return token
